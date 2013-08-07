7b4e3beecf8a0358cc4fe82e499bb0e556463c9c
alexizentner <alexiz19001@yahoo.com>
2013-08-07 12:20:48 -0400

    3rd revised and comments added

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
-var guess;
-var count = 0;
-var rand = Math.floor(Math.random()*100) + 1;
-var diff;
-$('#hot').hide();
-$('#cold').hide();
-console.log ("right number is ", rand)
+//set variables
+  var guess;
+  var count = 0;
+  var diff;
+  var rand = Math.floor(Math.random()*100) + 1;
+// check that computer number generated in console 
+  console.log ("right number is ", rand)
+// person playing guesses
 $("#guess").focus(function(){
 	$("#message").text("");
 	$(this).val("");
 })
-
 $("#submit").click(function(){
 	console.log("MADE A GUESS");
-guess = $("#guess").val();
+//guess made and now told higher/lower/correct guess
+   guess = $("#guess").val();
 	if (guess < 0 || guess > 100){
 		$("#message").text ("You must choose between 1 and 100!");
 	} else if (guess<rand) {
@@ -24,15 +25,15 @@ guess = $("#guess").val();
 	} else {
 		$("#message").text("Congratulations!! You guessed the number!");
 		 $("#diff").text("");
-
-	     
     }
+//count of guesses and printed
 	count = count + 1
 	if (count == 1) {
 		$("#count").text("1 Guess");
 	} else {
 		$("#count").text(count + " Guesses");
 	}
+//difference between guess and random and printed hot/cold and picture
 	diff=Math.abs(rand-guess)
 	if (diff >=1 && diff < 6){
 	   console.log ("HOT");
@@ -59,16 +60,17 @@ guess = $("#guess").val();
 
     }
 	});
-	
-    $("#Answer").click(function(){
+// button to get answer and hide other text and pic
+	  $("#Answer").click(function(){
     	$("#answer").text("The Answer is " + rand);
-    	$("#message").text("");
+    	  $("#message").text("");
 	      $("#guess").val("");
 	      $("#diff").text("");
 	      $('#hot').hide();
           $('#cold').hide();
 
     })
+// button to start new game, reset random number and hide text and pic
     $('#newgame').click(function(){
 	      count = 0
 	      $("#count").text("0 Guesses");
@@ -80,8 +82,8 @@ guess = $("#guess").val();
           $('#cold').hide();
          rand = Math.floor(Math.random()*100)+1;
          console.log("right number is ", rand);
-  
-  })
+    })
+//button to endgame, get answer and hide text and pics
     $("#endgame").click(function(){
     	  $("#answer").text("The Answer is " + rand);
           $("#message").text("");
